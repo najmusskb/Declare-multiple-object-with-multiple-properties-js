cd52fec853cdde67a7fcc5d68e44e2b5400b52fd
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-03 17:12:05 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,34 @@
+
+// object er simble version 
+
+
+var student1 = {
+    id: 70,
+    Name: 'sami',
+    marks: 81,
+    phone: 017222222,
+    height: 5.6,
+    department: CSE
+};
+
+var mobile = {
+
+    color: 'red',
+    configaration: 'redmi note 7 s',
+    ram: 8,
+    rom: 16,
+    harddisk: 32,
+    screenSize: 5
+};
+
+var book = {
+
+
+    name: 'programming.hero',
+    chapter: 16,
+    content: 78,
+    published: 'IEEE',
+    date: '12.03.22',
+    bookPublished: 'IEEE'
+
+};
